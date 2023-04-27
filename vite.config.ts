5e3979a7c5a082e3b905f69394c5d2ed03e754b8
gaoyang <xz@lgysh.cn>
2023-04-27 23:05:31 +0800

    add unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
 import { UserConfig } from 'vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import ssr from 'vite-plugin-ssr/plugin'
+import UnoCSS from 'unocss/vite'
 
 const config: UserConfig = {
   plugins: [
     vue(),
     ssr(),
+    UnoCSS(),
     AutoImport({
       resolvers: [ElementPlusResolver({ ssr: true })]
     }),
